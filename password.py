2ea1a2b53d00fc42ba7b613e70adaac11bc17ac7
Castiel Yu <yuche119@gmail.com>
2020-02-25 07:39:12 +0800

    chiang to while i > 0

--- password.py
+++ password.py
@@ -1,6 +1,20 @@
+# password = 'a123456'
+# i = 3
+# while True:
+#     pwd = input('請輸入密碼')
+#     if pwd == password:
+#         print('登入成功')
+#         break
+#     else:
+#         i = i - 1
+#         print('登入失敗！ 還有', i ,'次機會')
+#         if i == 0:
+#             break
+
+#進階挑戰
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
     pwd = input('請輸入密碼')
     if pwd == password:
         print('登入成功')
@@ -8,5 +22,3 @@ while True:
     else:
         i = i - 1
         print('登入失敗！ 還有', i ,'次機會')
-        if i == 0:
-            break
